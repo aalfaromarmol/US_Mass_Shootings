8a7d56dcb6c9673894b8d4941b8844af47c5fdcd
aalfaromarmol <aalfarom@uncc.edu>
2023-12-04 18:50:33 -0500

    changes made to project.py

---
+++ project.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+st.header('Visualizations of US Mass shootings Analysis (1983-2023)')
+
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+
+project = load_data('USMASS.csv')
+st.dataframe(project)
