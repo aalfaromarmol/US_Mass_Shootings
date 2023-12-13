014ee908ce7d53052dad0d6229f776d237d87e54
aalfaromarmol <aalfarom@uncc.edu>
2023-12-13 00:22:19 -0500

    changes

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # US_MassShootings
-Analyzing US Mass Shootings
+
+[![Open in Streamlit]()
+
+## Introduction
+This Streamlit dashboard enables users to view and analyze the relationship between mental health and mass gun violence in the US:
+- 
+- 
+
+
+
+History of Mass Shootings in the US : http://behindthetower.org/a-brief-history-of-mass-shootings 
+
+##Goal of the app 
+
+
+
+## Streamlit App
--- project.py
+++ project.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import altair as alt
+import pydeck as pdk
 
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.header('Visualizations of US Mass shootings Analysis (1983-2023)')
 
 def load_data(csv):
@@ -13,8 +15,67 @@ project = load_data('USMASS.csv')
 st.dataframe(project)
 
 # Load US Mass Shooting data and create a dataframe called mass
-mass_df = pd.read_csv(project)
+mass= pd.read_csv(project)
 
 # Display the columns and their data types
 print('Columns and their types:')
-print(mass_df.dtypes)
+print(mass.dtypes)
+
+st.header('Chart of US Mass shootings Analysis (1983-2023)')
+
+# Load US Mass Shooting data and create a dataframe called project
+project = load_data('USMASS.csv')
+st.dataframe(project)
+
+# Load US Mass Shooting data and create a dataframe called mass
+mass= pd.read_csv(project)
+
+# Create a bar chart
+chart = alt.Chart(mass).mark_bar().encode(
+    x='year:O',
+    y='fatalities:Q'
+)
+
+# Display the chart in Streamlit
+st.altair_chart(chart)
+
+# Load US Mass Shooting data and create a dataframe called mass
+mass= pd.read_csv('USMASS.csv')
+
+# Display the columns and their data types
+print('Columns and their types:')
+print(mass.dtypes)
+
+
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
+st.sidebar.header('Dashboard `US Mass Shooting ')
+
+#Create interactive map of US Mass Shootings
+st.header('Map of US Mass Shootings (1983-2023)')
+
+# data in 'Latitude' and 'Longitude' is in the form of decimal degrees
+map_data = project[['latitude', 'longitude']]
+
+# Define a layer to display on a map
+layer = pdk.Layer(
+    'ScatterplotLayer',
+    map_data,
+    get_position='[longitude, latitude]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=200,
+)
+
+# Set the map's initial viewport
+view_state = pdk.ViewState(
+    latitude=map_data['latitude'].mean(),
+    longitude=map_data['longitude'].mean(),
+    zoom=6,
+    pitch=0,
+)
+
+# Render
+r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+st.pydeck_chart(r)
