5d55c67a5f7bcc86fd96ecb53a8c795df0513175
aalfaromarmol <aalfarom@uncc.edu>
2023-12-14 17:53:15 -0500

    changes

--- project.py
+++ project.py
@@ -7,6 +7,11 @@ import streamlit as st
 import altair as alt
 import pydeck as pdk
 
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+
+
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.header('Visualizations of US Mass shootings Analysis (1983-2023)')
 
@@ -14,28 +19,19 @@ st.header('Visualizations of US Mass shootings Analysis (1983-2023)')
 file_path = "C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv"
 df = pd.read_csv(file_path)
 
-def load_data(csv):
-    df = pd.read_csv(csv)
-    return df
 
-project = load_data('USMASS.csv')
+project = load_data(file_path)
 st.dataframe(project)
 
-# Load US Mass Shooting data and create a dataframe called mass
-mass= pd.read_csv(project)
-
-# Display the columns and their data types
-print('Columns and their types:')
-print(mass.dtypes)
-
-st.header('Chart of US Mass shootings Analysis (1983-2023)')
+##Chart of US Mass shootings Analysis (1983-2023)
+st.header(' US Mass shootings Analysis (1983-2023)')
 
 # Load US Mass Shooting data and create a dataframe called project
-project = load_data('USMASS.csv')
+project = load_data("C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv")
 st.dataframe(project)
 
 # Load US Mass Shooting data and create a dataframe called mass
-mass= pd.read_csv(project)
+mass= pd.read_csv("C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv")
 
 # Create a bar chart
 chart = alt.Chart(mass).mark_bar().encode(
@@ -60,29 +56,48 @@ with open('style.css') as f:
     
 st.sidebar.header('Dashboard `US Mass Shooting ')
 
-#Create interactive map of US Mass Shootings
+# Set up Streamlit sidebar and header
+st.sidebar.header('Dashboard `US Mass Shooting')
 st.header('Map of US Mass Shootings (1983-2023)')
 
-# data in 'Latitude' and 'Longitude' is in the form of decimal degrees
+# Select relevant columns for the map
 map_data = project[['latitude', 'longitude']]
 
-# Define a layer to display on a map
+# Create PyDeck layer
 layer = pdk.Layer(
     'ScatterplotLayer',
-    map_data,
+    data=map_data,
     get_position='[longitude, latitude]',
-    get_color='[200, 30, 0, 160]',
     get_radius=200,
+    get_fill_color='[255, 0, 0]',
+    pickable=True,
 )
 
-# Set the map's initial viewport
+# Set initial view state
 view_state = pdk.ViewState(
     latitude=map_data['latitude'].mean(),
     longitude=map_data['longitude'].mean(),
-    zoom=6,
+    zoom=3,
     pitch=0,
 )
 
-# Render
-r = pdk.Deck(layers=[layer], initial_view_state=view_state)
-st.pydeck_chart(r)
+# Create PyDeck deck
+# Render PyDeck chart using st.pydeck_chart
+st.pydeck_chart(pdk.Deck(layers=[layer], initial_view_state=view_state))
+
+# Create Streamlit map
+st.map(map_data, use_container_width=True)
+
+# scatter data 
+file_path = "C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv"
+data = pd.read_csv(file_path)
+
+# Create a scatter plot
+scatter_plot = alt.Chart(data).mark_circle(size=60).encode(
+    x='year:Q',
+    y='injured:Q',
+    tooltip=['year', 'injured']
+)
+
+# Display the scatter plot in Streamlit
+st.altair_chart(scatter_plot)
