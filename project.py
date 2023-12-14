52918ea700d8a2b0a1488f62cde8abd639970ef0
aalfaromarmol <aalfarom@uncc.edu>
2023-12-14 18:31:03 -0500

    fix

--- project.py
+++ project.py
@@ -62,8 +62,9 @@ st.header('Map of US Mass Shootings (1983-2023)')
 
 # Select relevant columns for the map
 map_data = project[['latitude', 'longitude']]
+
 # Filter out null values
-map_data = map_data.dropna(subset=['latitude', 'longitude'])
+map_data2 = map_data.dropna(subset=['latitude', 'longitude'])
 
 # Create PyDeck deck
 deck = pdk.Deck(layers=alt.layer)
@@ -73,30 +74,33 @@ view_state = pdk.ViewState(latitude=37.7749, longitude=-122.4194, zoom=4, pitch=
 st.pydeck_chart(deck, initial_view_state=view_state)
 
 # Create Streamlit map
-st.map(map_data, use_container_width=True)
+st.map(map_data2, use_container_width=True)
 
-# scatter data 
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+# scatter data
 file_path = "C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv"
 data = pd.read_csv(file_path)
 
+# Define your DataFrame with scatter plot data
+plot_data = pd.data({
+    'year': [1983, 1984, 1985, 1986, 1987, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996,
+             1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013],
+    'injured': [1, 5, 10, 120, 30, 50, 15, 75, 90, 80, 60, 40, 20, 45, 70, 110, 25, 55, 85, 100, 65, 95, 35, 115, 130, 150, 140, 125, 135, 145]
+})
 
-data = pd.DataFrame({'year': [1983,1984,1985,1986,1987,1999,1989,1990,1991,1992,1993,1994,1995,1996,
-                              1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010, 2011, 2012, 2013],
-                     'injured': [1, 5, 10, 120]
-                     
-                     })
-# Create a range from min to max
-injured_range = range(min('injured'), max('injured') + 1)
-
-# Create a scatter plot
-scatter_plot = alt.Chart(data).mark_circle(
-    size=60, # controls the size of the circles
-    color='blue', # controls the color of the circles
-    opacity=0.5 # controls the transparency of the circles
+# Create a scatter plot using Altair
+scatter_plot = alt.Chart(plot_data).mark_circle(
+    size=60,  # controls the size of the circles
+    color='blue',  # controls the color of the circles
+    opacity=0.5  # controls the transparency of the circles
 ).encode(
     x='year:Q',
     y='injured:Q',
     tooltip=['year', 'injured']
 )
 
+# Display the Altair scatter plot in Streamlit
 st.altair_chart(scatter_plot)
