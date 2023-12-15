24d7781aeded7376e7a4a16afa1d807f72aee8f1
aalfaromarmol <aalfarom@uncc.edu>
2023-12-14 21:12:16 -0500

    ll

--- README.md
+++ README.md
@@ -4,15 +4,17 @@
 
 ## Introduction
 This Streamlit dashboard enables users to view and analyze the relationship between mental health and mass gun violence in the US:
-- 
-- 
-
-
+- The link between mental illness and mass gun violence
+- Importance of mental health 
+- Awareness of the relationship between these events and the mental health status of the shooters. 
 
 History of Mass Shootings in the US : http://behindthetower.org/a-brief-history-of-mass-shootings 
 
 ##Goal of the app 
 
-
+This app intends to visualize the growing problem of mass shootings in the US with data running back to the 1980s. 
+The app means to raise awareness and focus on mental health stigma. 
 
 ## Streamlit App
+
+
--- project.py
+++ project.py
@@ -1,56 +1,64 @@
-import numpy as np
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
 import pandas as pd
-import plotly.express as px 
 import streamlit as st
 import altair as alt
 import pydeck as pdk
+import plotly.express as px
+
+
 
 def load_data(csv):
     df = pd.read_csv(csv)
     return df
 
-
+# Set page config
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 st.header('Visualizations of US Mass shootings Analysis (1983-2023)')
 
-#load data
+# Sidebar
+st.sidebar.header('Dashboard US Mass Shooting ')
+
+# Load data
 file_path = "C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv"
-df = pd.read_csv(file_path)
-
-
 project = load_data(file_path)
 st.dataframe(project)
 
-##Chart of US Mass shootings Analysis (1983-2023)
-st.header(' US Mass shootings Analysis (1983-2023)')
+# Chart of US Mass shootings Analysis (1983-2023)
+st.header('Number of fatalities in US Mass shootings Analysis (1983-2023)')
 
-# Load US Mass Shooting data and create a dataframe called project
-project = load_data("C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv")
-st.dataframe(project)
-
-# Load US Mass Shooting data and create a dataframe called mass
-mass= pd.read_csv("C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv")
-
+# Create a bar chart
+chart1 = alt.Chart(project).mark_bar().encode(
+    x='year:O',
+    y='fatalities:Q'
+)
 
 # Display the chart in Streamlit
-st.altair_chart(chart)
+st.altair_chart(chart1)
 
-# Load US Mass Shooting data and create a dataframe called mass
-mass= pd.read_csv('USMASS.csv')
 
-# Display the columns and their data types
-print('Columns and their types:')
-print(mass.dtypes)
+# Signs of Mental Illness
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
-st.sidebar.header('Dashboard `US Mass Shooting ')
+# Chart of gunmen with prior signs of mental illness
+st.header('Gunmen with prior signs of mental illness vs. without prior signs of mental illness')
 
-# Set up Streamlit sidebar and header
-st.sidebar.header('Analysis of US Mass Shootings (1983-2023)')
+
+# Filter out null, 'Unknown', 'TBD', and 'Unclear' values
+filtered_project = project[
+    project['prior_signs_mental_health_issues'].notna() & 
+    (project['prior_signs_mental_health_issues'] != 'Unknown') &
+    (project['prior_signs_mental_health_issues'] != 'TBD') &
+    (project['prior_signs_mental_health_issues'] != 'Unclear')
+]
+
+# Create a bar chart using Altair
+chart2 = alt.Chart(filtered_project).mark_bar().encode(
+    alt.X('prior_signs_mental_health_issues:N', title='Mental Health Issues'),
+    alt.Y('count()', title='Count')
+)
+
+# Display the chart in Streamlit
+st.altair_chart(chart2)
+
+# Map
 st.header('Map of US Mass Shootings (1983-2023)')
 
 # Select relevant columns for the map
@@ -59,25 +67,18 @@ map_data = project[['latitude', 'longitude']]
 # Filter out null values
 map_data2 = map_data.dropna(subset=['latitude', 'longitude'])
 
-# Create PyDeck deck
-deck = pdk.Deck(layers=alt.layer)
-view_state = pdk.ViewState(latitude=37.7749, longitude=-122.4194, zoom=4, pitch=50)
-
-# Render PyDeck chart using st.pydeck_chart
-st.pydeck_chart(deck, initialview_state=view_state)
-
 # Create Streamlit map
 st.map(map_data2, use_container_width=True)
 
-# scatter data
-file_path = "C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv"
-data = pd.read_csv(file_path)
+# Chart of US Mass shootings Analysis (1983-2023)
+st.header('Number of injured in US Mass shootings Analysis (1983-2023)')
 
-# Define your DataFrame with scatter plot data
-plot_data = pd.data({
+# Scatter data
+plot_data = pd.DataFrame({
     'year': [1983, 1984, 1985, 1986, 1987, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996,
              1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013],
-    'injured': [1, 5, 10, 120, 30, 50, 15, 75, 90, 80, 60, 40, 20, 45, 70, 110, 25, 55, 85, 100, 65, 95, 35, 115, 130, 150, 140, 125, 135, 145]
+    'injured': [1, 5, 10, 120, 30, 50, 15, 75, 90, 80, 60, 40, 20, 45, 70, 110, 25, 55, 85, 100, 65, 95,
+                 35, 115, 130, 150, 140, 125, 135, 145]
 })
 
 # Create a scatter plot using Altair
@@ -86,7 +87,7 @@ scatter_plot = alt.Chart(plot_data).mark_circle(
     color='blue',  # controls the color of the circles
     opacity=0.5  # controls the transparency of the circles
 ).encode(
-    x='year:Q',
+    x='year:O',
     y='injured:Q',
     tooltip=['year', 'injured']
 )
@@ -94,28 +95,23 @@ scatter_plot = alt.Chart(plot_data).mark_circle(
 # Display the Altair scatter plot in Streamlit
 st.altair_chart(scatter_plot)
 
-
-# density data
-
-df = pd.read_csv("C:/Users/gram/OneDrive/Documents/5122/5122 FINAL/US_Mass_Shootings/USMASS.csv")
-
-fig = px.scatter(
-    df.query('year'),  # Add closing parenthesis here
-    x="injuries",
-    y="year",
-    size="pop",
-    color="injuries",
-    hover_name="total_victims",
-    log_x=True,
-    size_max=60,
-)
+# Define a list of charts
+charts = ['Select...', 'Number of fatalities in US Mass shootings Analysis (1983-2023)',
+          'Gunmen with prior signs of mental illness vs. without prior signs of mental illness',
+          'Number of injured in US Mass shootings Analysis (1983-2023)']
 
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-with tab1:
-    # Use the Streamlit theme.
-    # This is the default. So you can also omit the theme argument.
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-with tab2:
-    # Use the native Plotly theme.
-    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+# Create a dropdown menu for selecting a chart
+selected_chart = st.selectbox('Select a chart', charts)
+
+# Display the selected chart
+if selected_chart == 'Number of fatalities in US Mass shootings Analysis (1983-2023)':
+    # Display the first chart
+    st.altair_chart(chart1)
+elif selected_chart == 'Gunmen with prior signs of mental illness vs. without prior signs of mental illness':
+    # Display the second chart
+    st.altair_chart(chart2)
+elif selected_chart == 'Number of injured in US Mass shootings Analysis (1983-2023)':
+    # Display the third chart
+    st.altair_chart(scatter_plot)
