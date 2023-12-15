5924cc3cfe6a77f357f299835203d4570dba0d93
aalfaromarmol <aalfarom@uncc.edu>
2023-12-14 19:19:25 -0500

    update

--- project2.py
+++ project2.py
@@ -47,9 +47,10 @@ map_data2 = map_data.dropna(subset=['latitude', 'longitude'])
 # Create PyDeck deck
 deck = pdk.Deck(layers=[pdk.Layer(type='ScatterplotLayer', data=map_data2)])
 
+view_state = pdk.ViewState(latitude=37.7749, longitude=-122.4194, zoom=4, pitch=50)
 
 # Render PyDeck chart using st.pydeck_chart
-st.pydeck_chart(deck, initial_view_state=view_state)
+st.pydeck_chart(deck, initialViewState=view_state) 
 
 # Create Streamlit map
 st.map(map_data2, use_container_width=True)
